38d6d78e785b2387072dd5b7fe399670b39249a3
Claudio Mattera <claudio@mattera.it>
2016-10-15 17:26:55 +0200

    Improve std::bitset implementation

--- bitset.h
+++ bitset.h
@@ -1,5 +1,6 @@
 #include <cstdlib> // For std::size_t
 #include <climits> // For CHAR_BIT
+#include <stdexcept> // For std::out_of_range
 
 template <std::size_t SIZE>
 class bitset
@@ -21,8 +22,18 @@ public:
 
         reference & operator=(bool const value)
         {
-            object.set(position, value);
+            object.set_value(position, value);
             return *this;
+        }
+
+        bool operator~() const
+        {
+            return ~object.get(position);
+        }
+
+        void flip()
+        {
+            object.set_value(position, ~object.get(position));
         }
 
     private:
@@ -30,11 +41,31 @@ public:
         std::size_t const position;
     };
 
-    bitset(unsigned char value = 0)
+    constexpr bitset()
+    : data{0}
+    {
+    }
+
+    bitset(unsigned long long value)
     {
         for (std::size_t position = 0; position < SIZE; ++position) {
-            set(position, 1 & (value >> position));
+            (*this)[position] = 1 & (value >> position);
         }
+    }
+
+    bool operator==(const bitset<SIZE> & that) const
+    {
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            if (this->data[i] != that.data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator!=(const bitset<SIZE> & that) const
+    {
+        return ! (*this == that);
     }
 
     unsigned long to_ulong() const
@@ -46,22 +77,94 @@ public:
         return result;
     }
 
-    bool operator[](std::size_t position) const
+    constexpr bool operator[](std::size_t position) const
     {
         return get(position);
     }
 
     bool test(std::size_t position) const
     {
+        ensure_in_range(position);
         return get(position);
     }
 
-    bool get(std::size_t position) const
+    constexpr bool get(std::size_t position) const
     {
-        const size_t byte_index = position / CHAR_BIT;
-        const size_t offset = position % CHAR_BIT;
-        const unsigned char byte = data[byte_index];
-        return 1 & (byte >> offset);
+        return 1 & (data[position / CHAR_BIT] >> (position % CHAR_BIT));
+    }
+
+    bool all() const
+    {
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            if (!get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool any() const
+    {
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            if (get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool none() const
+    {
+        return !any();
+    }
+
+    std::size_t count() const
+    {
+        std::size_t c = 0;
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            if (get(i)) {
+                c += 1;
+            }
+        }
+        return c;
+    }
+
+    constexpr std::size_t size() const
+    {
+        return SIZE;
+    }
+
+    bitset<SIZE> & operator&=(const bitset<SIZE> & that)
+    {
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            (*this)[i] = this->get(i) & that.get(i);
+        }
+        return *this;
+    }
+
+    bitset<SIZE> & operator|=(const bitset<SIZE> & that)
+    {
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            (*this)[i] = this->get(i) | that.get(i);
+        }
+        return *this;
+    }
+
+    bitset<SIZE> & operator^=(const bitset<SIZE> & that)
+    {
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            (*this)[i] = this->get(i) ^ that.get(i);
+        }
+        return *this;
+    }
+
+    bitset<SIZE> operator~() const
+    {
+        bitset<SIZE> result;
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            result[i] = ~this->get(i);
+        }
+        return result;
     }
 
     reference operator[](std::size_t position)
@@ -69,13 +172,66 @@ public:
         return reference(*this, position);
     }
 
-    void set(std::size_t position, bool value)
+    bitset<SIZE> & set()
+    {
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            set_value(i, true);
+        }
+        return *this;
+    }
+
+    bitset<SIZE> & set(std::size_t position, bool value = true)
+    {
+        ensure_in_range(position);
+        set_value(position, value);
+        return *this;
+    }
+
+    bitset<SIZE> & reset()
+    {
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            set_value(i, false);
+        }
+        return *this;
+    }
+
+    bitset<SIZE> & reset(std::size_t position)
+    {
+        ensure_in_range(position);
+        set_value(position, false);
+        return *this;
+    }
+
+    bitset<SIZE> & flip()
+    {
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            set_value(i, ~get(i));
+        }
+        return *this;
+    }
+
+    bitset<SIZE> & flip(std::size_t position)
+    {
+        ensure_in_range(position);
+        set_value(position, ~get(position));
+        return *this;
+    }
+
+    void set_value(std::size_t position, bool value)
     {
         const size_t byte_index = position / CHAR_BIT;
         const size_t offset = position % CHAR_BIT;
         const unsigned char byte = data[byte_index];
         const unsigned char value_c = value;
         data[byte_index] ^= (-value_c ^ byte) & (1 << offset);
+    }
+
+private:
+    void ensure_in_range(std::size_t position) const
+    {
+        if (position < 0 || position > SIZE) {
+            throw new std::out_of_range("Out of range");
+        }
     }
 
 private:
